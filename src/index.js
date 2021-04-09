ca9a45fce89bbcb353fa1aa8dc0b2b6b2ca2e90a
AndreMicheletti <andre.micheletti@zenvia.com>
2021-04-09 16:58:14 -0300

    pwa

--- src/index.js
+++ src/index.js
@@ -12,6 +12,22 @@ import configureStore from './store'
 
 const store = configureStore();
 
+export const registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        // Registration was successful
+        //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function (err) {
+        // registration failed :(
+        //console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
+
+registerServiceWorker()
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
