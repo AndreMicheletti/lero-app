a94efea0e4942fd2441869517658a1e68ab034b9
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2021-04-12 23:51:22 -0300

    prod envs

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 
 // https://polar-ridge-40128.herokuapp.com
-export const BACKEND_URL = "http://192.168.15.29:4000"
-export const WEBSOCKET_URL = "ws://192.168.15.29:4000/socket"
+export const BACKEND_URL = "https://polar-ridge-40128.herokuapp.com"
+export const WEBSOCKET_URL = "wss://polar-ridge-40128.herokuapp.com/socket"
